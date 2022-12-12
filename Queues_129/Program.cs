40b9978f813e0b1b39952f8798c98f5938cce985
Raihan454 <raihanmarsa712@gmail.com>
2022-12-12 16:39:14 +0700

    add public Program

--- Queues_129/Program.cs
+++ Queues_129/Program.cs
@@ -11,6 +11,13 @@ namespace Queues_129
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
 
+        public Program()
+        {
+            /*Initializing the values of the variables REARand FRONT to -1 to indicate that
+             * the queue is initially empty.*/
+            FRONT = -1;
+            REAR = -1;
+        }
         static void Main(string[] args)
         {
         }
