a7d703152864310f9aaa2efd9a5db6dc79ab5af2
Raihan454 <raihanmarsa712@gmail.com>
2022-12-12 16:38:56 +0700

    add class Program

--- Queues_129/Program.cs
+++ Queues_129/Program.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 
 namespace Queues_129
 {
-    internal class Program
+    class Program
     {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+
         static void Main(string[] args)
         {
         }
